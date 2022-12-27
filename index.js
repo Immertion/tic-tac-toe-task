bc7d282ef1bb0a18c59904618d14149c3a3e70e4
Artyom Kurpas <minicoal@gmail.com>
2022-12-27 22:00:01 +1000

    Npm init, Update docs, Add main page

---
+++ index.js
@@ -0,0 +1,25 @@
+// Зависимости
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var socketIO = require('socket.io');
+var app = express();
+var server = http.Server(app);
+var io = socketIO(server);
+
+app.set('port', 5000);
+app.use('/static', express.static(__dirname + '/static'));
+
+app.use(
+    express.static(path.join(__dirname, "node_modules/bootstrap/dist/"))
+);
+
+// Маршруты
+app.get('/', function(request, response) {
+    response.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Запуск сервера
+server.listen(5000, function() {
+    console.log('Запускаю сервер на порте 5000');
+});
