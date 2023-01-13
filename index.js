ac192cc051edf12ad8ae3e45ce4a96a52cedd48c
Artyom Kurpas <minicoal@gmail.com>
2023-01-14 04:45:45 +1000

    Add rooms

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 // Зависимости
-var express = require('express');
-var http = require('http');
-var path = require('path');
-var socketIO = require('socket.io');
-var app = express();
-var server = http.Server(app);
-var io = socketIO(server);
+const express = require('express');
+const { query } = require('express');
+
+const http = require('http');
+const path = require('path');
+const socketIO = require('socket.io');
+const app = express();
+const server = http.Server(app);
+
+const io = socketIO(server);
 
 // Настройки сервера
 const settings = require('./settings.js');
@@ -13,12 +16,21 @@ const settings = require('./settings.js');
 app.set('port', settings.serverPort);
 app.use('/static', express.static(__dirname + '/static'));
 
+// Комнаты
+let rooms = [];
+
 // Маршруты
-app.get('/', (request, response) => {
-    response.sendFile(path.join(__dirname, './static/index.html'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './static/index.html'));
 });
-app.get('/game', (request, response) => {
-    response.sendFile(path.join(__dirname, './static/routes/game.html'));
+app.get('/game', (req, res) => {
+    // Перенаправление на главную страницу, в случае,
+    // если такого id нет
+    // if(!rooms.includes(req.query['id'])) {
+    //     res.redirect('/');
+    // }
+
+    res.sendFile(path.join(__dirname, './static/routes/game.html'));
 });
 
 // Запуск сервера
@@ -26,8 +38,29 @@ server.listen(5000, settings.serverIP, () => {
     console.log('Запускаю сервер на порте 5000');
 });
 
-var players = {};
+io.on('connection', (socket) => {
+    socket.on('createRoom', (roomName) => {
+        roomObj = {roomName: roomName, playerName: undefined};
+        rooms.push(roomObj);
+        console.log(rooms);
+    });
 
-io.on("connection", (socket) => {
-    console.log(socket.id);
+    socket.on('findRoom', (roomName) => {
+        let response = true;
+        // if (rooms.find(element => element === roomName)) {
+        //     response = true;
+        // }
+        
+        socket.emit('roomExists', response);
+    });
+
+    socket.on('addPlayerToRoom', (roomObj) => {
+        if (!rooms.find(element => element[roomName])) {
+            socket.emit('roomExists', false);
+            return;
+        }
+
+        rooms.find(element => element = roomObj.roomName)['playerName'] = roomObj.playerName;
+        console.log(rooms);
+    })
 });
